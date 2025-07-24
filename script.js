904ab0f4006013eefbd405c5a98b5470302fa944
Mosquera-20255 <vivedigital@mosquera-cundinamarca.gov.co>
2025-07-24 14:12:02 -0500

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const darkLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        subdomains: 'abcd', maxZoom: 10, detectRetina: true
+        subdomains: 'abcd', maxZoom: 30, detectRetina: true
     });
     const lightLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
